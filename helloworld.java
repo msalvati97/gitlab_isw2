00e4a506a9e6f4bbc0272134b92af925edca3cbb
msalvati97 <73780515+msalvati97@users.noreply.github.com>
2021-03-12 11:57:59 +0100

    Issues resolved (package+system.out)

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,9 @@
-package main;
+package trunk
 /* This is a simple Java program for git labs */
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		logger.debug("Hello,world");
 	}
 
 }
